1dfc3d9da4b932438689586243c471cf2ae512b9
Lemon <2456844450@qq.com>
2021-03-18 10:43:28 +0800

    daily algorithm plan

---
+++ 字符串中求最大不重复字符串.js
@@ -0,0 +1,49 @@
+//教材
+function func(str) {
+  let a,
+    b = 0;
+
+  let s = new Set();
+
+  for (a = 0; a < str.length; a++) {
+    if (a !== 0) {
+      s.delete(str[a - 1]);
+    }
+    //a是左指针
+    while (b < str.length && !s.has(str[b])) {
+      s.add(str[b]);
+      b++;
+    }
+    console.log("s", s);
+    if (b == str.length) {
+      break;
+    }
+  }
+}
+
+//自己
+function func(str) {
+  let j = 0;
+  let n = str.length;
+  let res = "";
+  let r = "";
+  while (1) {
+    while (j < n && res.indexOf(str[j]) === -1) {
+      res += str[j];
+      j++;
+    }
+    if (res.length > r.length) {
+      r = res;
+      console.log("r", r);
+    }
+    if (j < n) {
+      //找到重复的了
+      res = res.slice(res.indexOf(str[j]) + 1);
+    } else {
+      break;
+    }
+  }
+  return r;
+}
+
+func("aasdasasfqwe");
